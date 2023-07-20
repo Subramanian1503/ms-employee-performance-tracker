c16c5a1130590e9c683838d29e0fe97d3cb7a179
subramanian1503 <roopan1503@gmail.com>
2023-07-20 18:10:59 +0530

    Created User model to create users information in DB

---
+++ model/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+// Defining the schema of the user
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["EMPLOYEE", "ADMIN"],
+    default: "EMPLOYEE",
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    required: true,
+  },
+  dateOfJoining: {
+    type: Date,
+    required: true,
+  },
+});
+
+// Converting the schema into model
+const User = mongoose.model("User", userSchema);
+
+// Exporting the created model
+module.exports = User;
